63666f0e4261d4c845ba141fe3eaf05b416e2058
kuugu <kuugu.work@gmail.com>
2025-04-01 22:49:12 +0530

    new straight layout branch

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
-console.log('Hello World to you')
+let letters = [
+    'ಅ', 'ಆ', 'ಇ', 'ಈ', 'ಉ', 'ಊ', 'ಋ', 'ಎ', 'ಏ', 'ಐ', 'ಒ', 'ಓ', 'ಔ', 'ಅಂ', 'ಅಃ', 
+    'ಕ', 'ಖ', 'ಗ', 'ಘ', 'ಙ' , 
+    'ಚ', 'ಛ', 'ಜ', 'ಝ', 'ಞ' , 
+    'ಟ', 'ಠ', 'ಡ', 'ಢ', 'ಣ' , 
+    'ತ', 'ಥ', 'ದ', 'ಧ', 'ನ' , 
+    'ಪ', 'ಫ', 'ಬ', 'ಭ', 'ಮ', 
+    'ಯ', 'ರ', 'ಲ', 'ವ', 'ಶ', 'ಷ', 'ಸ', 'ಹ', 'ಳ'
+]; 
+
+for (let i=0; i<letters.length; i++) {
+    let curr_elem = document.createElement('div');
+    let link = document.createElement('a'); 
+    
+    link.className = "letter-link"; 
+    link.href = "pages/" + letters[i] + ".html"; 
+    link.textContent = letters[i];
+    
+    curr_elem.className = "letter"; 
+    curr_elem.appendChild(link); 
+
+    document.getElementById("letters")!.appendChild(curr_elem); 
+}
