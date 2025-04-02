42e079de6265984be08bcc93935f996e77149b65
kuugu <kuugu.work@gmail.com>
2025-04-03 00:40:53 +0530

    search bar, final layout, colors

---
+++ src/letters.ts
@@ -0,0 +1,40 @@
+export function create_letters() {
+    const letters = [
+        ['ಅ', 'ಆ', 'ಇ', 'ಈ', 'ಉ', 'ಊ', 'ಋ', 'ಎ', 'ಏ', 'ಐ', 'ಒ', 'ಓ', 'ಔ', 'ಅಂ', 'ಅಃ'], 
+        ['ಕ', 'ಖ', 'ಗ', 'ಘ', 'ಙ'], 
+        ['ಚ', 'ಛ', 'ಜ', 'ಝ', 'ಞ'], 
+        ['ಟ', 'ಠ', 'ಡ', 'ಢ', 'ಣ'], 
+        ['ತ', 'ಥ', 'ದ', 'ಧ', 'ನ'], 
+        ['ಪ', 'ಫ', 'ಬ', 'ಭ', 'ಮ'], 
+        ['ಯ', 'ರ', 'ಲ', 'ವ', 'ಶ', 'ಷ', 'ಸ', 'ಹ', 'ಳ'] 
+    ]; 
+    
+    const letters_done = new Set<string>(['ಔ']); 
+    
+    
+    for (let i=0; i<letters.length; i++) {
+        let row = document.createElement('div'); 
+        row.className = "letters-row"; 
+    
+        for (let j=0; j<letters[i].length; j++) {
+            let curr_elem = document.createElement('div');
+            let link = document.createElement('a'); 
+            
+            if (letters_done.has(letters[i][j])) {
+                link.className = "letter-link-done"; 
+                link.href = "pages/" + letters[i][j] + ".html"; 
+            } else {
+                link.className = "letter-link-yts"; 
+            }
+    
+            link.textContent = letters[i][j];
+            
+            curr_elem.className = "letter"; 
+            curr_elem.appendChild(link); 
+    
+            row.appendChild(curr_elem); 
+        }
+    
+        document.getElementById("letters")!.appendChild(row); 
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,6 @@
-let letters = [
-    'ಅ', 'ಆ', 'ಇ', 'ಈ', 'ಉ', 'ಊ', 'ಋ', 'ಎ', 'ಏ', 'ಐ', 'ಒ', 'ಓ', 'ಔ', 'ಅಂ', 'ಅಃ', 
-    'ಕ', 'ಖ', 'ಗ', 'ಘ', 'ಙ' , 
-    'ಚ', 'ಛ', 'ಜ', 'ಝ', 'ಞ' , 
-    'ಟ', 'ಠ', 'ಡ', 'ಢ', 'ಣ' , 
-    'ತ', 'ಥ', 'ದ', 'ಧ', 'ನ' , 
-    'ಪ', 'ಫ', 'ಬ', 'ಭ', 'ಮ', 
-    'ಯ', 'ರ', 'ಲ', 'ವ', 'ಶ', 'ಷ', 'ಸ', 'ಹ', 'ಳ'
-]; 
+import {create_letters} from './letters.ts'; 
 
-let letter_wip = 'ಅಂ'; 
+// render the letters 
+create_letters(); 
 
-const letters_done = new Set<string>(['ಔ']); 
-
-
-for (let i=0; i<letters.length; i++) {
-    let curr_elem = document.createElement('div');
-    let link = document.createElement('a'); 
-    
-    if (letters_done.has(letters[i])) {
-        link.className = "letter-link-done"; 
-        link.href = "pages/" + letters[i] + ".html"; 
-    } else if (letter_wip === letters[i]) {
-        link.className = "letter-link-wip"; 
-    } else {
-        link.className = "letter-link-yts"; 
-    }
-
-    link.textContent = letters[i];
-    
-    curr_elem.className = "letter"; 
-    curr_elem.appendChild(link); 
-
-    document.getElementById("letters")!.appendChild(curr_elem); 
-}
+// 
